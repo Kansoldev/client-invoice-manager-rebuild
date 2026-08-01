5948b5303fb7443f6c146cc6db7c148871676d02
Oyinkansola <yahayaoyinkansola@gmail.com>
2026-08-01 15:52:26 +0100

    feat: make view invoice page mobile responsive

--- src/components/ViewInvoice.tsx
+++ src/components/ViewInvoice.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import { Link, useParams, useOutletContext } from "react-router-dom";
 import { ChevronLeft } from "lucide-react";
 import { cn } from "@/lib/utils";
@@ -21,26 +22,28 @@ const ViewInvoice = () => {
 
   return (
     <>
-      <section>
-        <Link
-          to="/"
-          className="inline-flex items-center gap-4 ml-64 mt-20 font-semibold text-sm text-primary"
-        >
-          <ChevronLeft
-            size={18}
-            className="text-secondary hidden sm:block cursor-pointer mb-1"
-          />{" "}
-          Go back
-        </Link>
+      <section className="px-6 mt-20">
+        <div className="lg:w-210 mx-auto">
+          <Link
+            to="/"
+            className="inline-flex items-center gap-4 mt-10 mb-5 font-semibold text-sm text-primary"
+          >
+            <ChevronLeft
+              size={18}
+              className="text-secondary cursor-pointer mb-1"
+            />{" "}
+            Go back
+          </Link>
+        </div>
 
         {filteredInvoice.map((invoice: invoiceFormProps) => (
-          <>
-            <div className="bg-invoice flex lg:w-210 mx-auto items-center justify-between gap-2 mt-8 py-6 px-9 rounded-[10px]">
-              <div className="flex items-center gap-5">
+          <Fragment key={invoice.invoiceId}>
+            <div className="bg-invoice flex items-center justify-center md:justify-between lg:w-210 mx-auto py-6 px-6 md:px-9 rounded-[10px]">
+              <div className="flex items-center justify-between md:justify-start w-full md:w-auto gap-5">
                 <span className="text-pastel-indigo font-medium">Status</span>
                 <span
                   className={cn(
-                    "inline-block px-11 py-3 rounded-md col-start-2 sm:order-4 relative top-4 sm:top-0 font-bold before:content-[''] before:absolute before:top-4.75 before:bottom-0 before:left-7 before:w-2 before:h-2 before:rounded-full",
+                    "inline-block px-11 py-3 rounded-md col-start-2 sm:order-4 font-bold before:content-[''] before:absolute before:top-4.75 before:bottom-0 before:left-7 before:w-2 before:h-2 before:rounded-full",
                     checkStatus(invoice.status, darkMode),
                   )}
                 >
@@ -48,7 +51,7 @@ const ViewInvoice = () => {
                 </span>
               </div>
 
-              <div className="flex gap-2">
+              <div className="hidden md:flex gap-2">
                 <Button
                   size="lg"
                   className="text-[15px] bg-strong-white dark:bg-comet text-secondary dark:text-white"
@@ -66,20 +69,18 @@ const ViewInvoice = () => {
               </div>
             </div>
 
-            <div className="bg-invoice w-210 mt-10 shadow-lg mx-auto rounded-[10px] py-6 px-9">
-              <div className="flex justify-between">
+            <div className="bg-invoice lg:w-210 mt-10 shadow-lg mx-auto rounded-[10px] py-6 px-6">
+              <div className="md:flex justify-between">
                 <div>
                   <div className="font-bold text-primary mb-2">
                     <span className="text-glaucous">#</span>
                     <span className="uppercase">{invoice.invoiceId}</span>
                   </div>
 
-                  <p className="text-label w-2/3">
-                    {invoice.projectDescription}
-                  </p>
+                  <p className="text-label">{invoice.projectDescription}</p>
                 </div>
 
-                <div className="text-right text-label">
+                <div className="md:text-right text-label mt-6 md:mt-0">
                   <p>{invoice.fromAddress}</p>
                   <p>{invoice.fromCity}</p>
                   <p>{invoice.fromPostCode}</p>
@@ -87,7 +88,7 @@ const ViewInvoice = () => {
                 </div>
               </div>
 
-              <div className="flex gap-40 mt-20">
+              <div className="grid grid-cols-2 md:grid-cols-3 mt-12">
                 <div>
                   <div>
                     <span className="text-label block">Invoice Date</span>
@@ -118,9 +119,45 @@ const ViewInvoice = () => {
                 </div>
               </div>
 
-              <div className="bg-strong-white dark:bg-comet rounded-[10px] mt-10 overflow-hidden">
+              <div className="bg-strong-white dark:bg-comet rounded-2xl mt-16 md:hidden">
+                <div className="p-6">
+                  {invoice.invoiceItems.map((item) => (
+                    <div
+                      key={item.id}
+                      className={`flex items-center justify-between ${Number(item.id) !== 1 ? "mt-5" : ""}`}
+                    >
+                      <div>
+                        <h4 className="text-primary font-bold">
+                          {item.itemName}
+                        </h4>
+                        <p className="text-glaucous font-bold">
+                          {item.itemQuantity} x £{" "}
+                          {Number(item.itemPrice).toFixed(2)}
+                        </p>
+                      </div>
+
+                      <p className="text-primary font-bold">
+                        £ {Number(item.total).toFixed(2)}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+
+                <div className="bg-tranquil dark:bg-black-pepper flex items-center justify-between mt-5 p-6 rounded-br-[10px] rounded-bl-[10px]">
+                  <p className="text-white text-sm">Grand Total</p>
+                  <p className="text-white font-bold text-xl min-[25rem]:text-3xl md:text-base">
+                    £
+                    {TotalItemPrices.reduce(
+                      (acc, currentValue) => acc + currentValue,
+                      0,
+                    ).toFixed(2)}
+                  </p>
+                </div>
+              </div>
+
+              <div className="bg-strong-white dark:bg-comet rounded-2xl mt-16 hidden md:block">
                 <table className="w-full">
-                  <thead>
+                  <thead className="hidden md:block">
                     <tr className="grid grid-cols-5 w-full p-10">
                       <th className="text-left text-label font-medium ml-3 col-span-2">
                         Item Name
@@ -133,7 +170,10 @@ const ViewInvoice = () => {
 
                   <tbody className="flex flex-col mt-2">
                     {invoice.invoiceItems.map((item) => (
-                      <tr className="grid grid-cols-5 gap-4 mb-6 text-center px-10">
+                      <tr
+                        key={item.id}
+                        className="grid grid-cols-5 gap-4 mb-6 text-center px-10"
+                      >
                         <td className="col-span-2 text-left">
                           <span className="font-bold text-primary">
                             {item.itemName}
@@ -189,7 +229,7 @@ const ViewInvoice = () => {
                 </table>
               </div>
             </div>
-          </>
+          </Fragment>
         ))}
       </section>
     </>
