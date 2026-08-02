72b97427651e267072e519070c1a683d142a24a3
Oyinkansola <yahayaoyinkansola@gmail.com>
2026-08-02 01:11:59 +0100

    refactor: read the correct count of invoices

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
+import { useOutletContext } from "react-router-dom";
 import { PlusCircle } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import type { OutletContext } from "@/types";
 import {
   Select,
   SelectContent,
@@ -16,6 +18,7 @@ const Header = ({
   onToggleAddInvoiceModal: () => void;
 }) => {
   const isPhone = useMediaQuery("(max-width: 640px)");
+  const { invoices } = useOutletContext<OutletContext>();
 
   return (
     <header className="text-center mt-20 w-full lg:w-200 mx-auto px-4 py-6 lg:p-0 flex items-center justify-between">
@@ -24,7 +27,9 @@ const Header = ({
           Invoices
         </h1>
 
-        <p className="text-paragraph text-sm lg:text-base">No invoices</p>
+        <p className="text-paragraph text-sm lg:text-base">
+          {invoices.length > 0 ? `${invoices.length} Invoices` : "No invoices"}
+        </p>
       </div>
 
       <div className="flex items-center lg:gap-15">
